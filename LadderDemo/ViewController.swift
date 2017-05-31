7fa5ea250a050cb5f0654022f51131d1ec4b9972
rmelps <RMelpignano@richards-mbp.home>
2017-05-31 17:37:47 -0400

    Initial Commit

---
+++ LadderDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LadderDemo
+//
+//  Created by Richard Melpignano on 5/31/17.
+//  Copyright © 2017 J2MFD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
